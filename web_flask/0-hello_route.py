2acaed7c0155efa7d03b29002ce79ab8312ad050
Abah-Emmanuel-Adah11 <abahemmanueladah11@gmail.com>
2024-01-02 13:29:35 -0800

    Update to Task 0

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,35 +1,16 @@
 #!/usr/bin/python3
-"""
-<<<<<<< HEAD
-A script that starts a Flask web application:
-=======
-starting Flask web application
->>>>>>> f44ee2d87fc5aa53b6123fb4f099817475fd3c05
-"""
+""" A Script to start a Flask web application """
 
 from flask import Flask
+
 
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-<<<<<<< HEAD
-def hello_route():
-    """
-    Displays 'Hello HBNB!'
-    Returns:
-        str: "Hello HBNB"
-    """
-    return "Hello HBNB!"
-
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
-=======
-def hello_hbnb():
-    """ To return Hello HBNB!"""
+def hello_world():
+    """ Returns some text. """
     return 'Hello HBNB!'
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
->>>>>>> f44ee2d87fc5aa53b6123fb4f099817475fd3c05
