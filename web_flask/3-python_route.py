271acf8b8d3960af91c77d372d3376ac930873f0
Abah-Emmanuel-Adah11 <abahemmanueladah11@gmail.com>
2024-01-03 14:10:03 -0800

    Updates

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-<<<<<<< HEAD
 """
 A script that starts a Flask web application:
 """
@@ -55,43 +54,3 @@ def python_route(text="is_cool"):
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
-=======
-""" It add third view func that redirects and has default val for variable """
-
-from flask import Flask
-
-
-app = Flask(__name__)
-app.url_map.strict_slashes = False
-
-
-@app.route('/')
-def hello_world():
-    """ Returns some text. """
-    return 'Hello HBNB!'
-
-
-@app.route('/hbnb')
-def hello():
-    """ Return other text. """
-    return 'HBNB'
-
-
-@app.route('/c/<text>')
-def c_text(text):
-    """ replace text with variable. """
-    text = text.replace('_', ' ')
-    return 'C {}'.format(text)
-
-
-@app.route('/python/')
-@app.route('/python/<text>')
-def python_text(text='is cool'):
-    """ replacing more text with another variable. """
-    text = text.replace('_', ' ')
-    return 'Python {}'.format(text)
-
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
->>>>>>> f44ee2d87fc5aa53b6123fb4f099817475fd3c05
