c1a366e1075dff0d68f466b6d6e2651ac37b8f69
Abah-Emmanuel-Adah11 <abahemmanueladah11@gmail.com>
2024-01-03 14:23:46 -0800

    Task 5 updated

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,16 +1,11 @@
 #!/usr/bin/python3
-<<<<<<< HEAD
 """
 A script that starts a Flask web application:
 """
-=======
-""" it add fifth view func that displays HTML page if n is int """
->>>>>>> f44ee2d87fc5aa53b6123fb4f099817475fd3c05
 
 from flask import Flask
 from flask import render_template
 
-<<<<<<< HEAD
 app = Flask(__name__)
 
 
@@ -81,53 +76,3 @@ def number_template_route(n):
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
-=======
-
-app = Flask(__name__)
-app.url_map.strict_slashes = False
-
-
-@app.route('/')
-def hello_world():
-    """ Returns some text. """
-    return 'Hello HBNB!'
-
-
-@app.route('/hbnb')
-def hello():
-    """ Return other text. """
-    return 'HBNB'
-
-
-@app.route('/c/<text>')
-def c_text(text):
-    """ replace text with variable. """
-    text = text.replace('_', ' ')
-    return 'C {}'.format(text)
-
-
-@app.route('/python/')
-@app.route('/python/<text>')
-def python_text(text='is cool'):
-    """ replacing more text with another variable. """
-    text = text.replace('_', ' ')
-    return 'Python {}'.format(text)
-
-
-@app.route('/number/<int:n>')
-def number_text(n):
-    """ replace with int only if given int. """
-    n = str(n)
-    return '{} is a number'.format(n)
-
-
-@app.route('/number_template/<int:n>')
-def html_num(n):
-    """ display html if n is int. """
-    n = str(n)
-    return render_template('5-number.html', n=n)
-
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
->>>>>>> f44ee2d87fc5aa53b6123fb4f099817475fd3c05
